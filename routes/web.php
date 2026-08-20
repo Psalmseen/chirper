a0c9ba4d2c85d028f18ad57e5b325652789398ee
Samson-Irecharge <oyebamiji.samson@irecharge.ng>
2026-08-20 09:56:51 +0100

    implement authentication features including user registration, logout, and chirp management

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ChirperController;
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\Auth\Logout;
 
-Route::get('/', [ChirperController::class, 'index']);
+Route::get('/', [ChirpController::class, 'index']);
+
+Route::middleware('auth')->group(function () {
+    Route::post('/chirps', [ChirpController::class, 'store']);
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+    Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+    Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+});
+
+
+// REGISTER ROUTE
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
+
+
+
+// LOGOUT ROUTE
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
